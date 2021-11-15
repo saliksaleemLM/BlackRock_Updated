87ae814f512798c72d1b570e2c36438be5832beb
SalikSaleemLM <salik.saleem@lampromellon.com>
2021-11-15 17:36:22 +0500

    range slider styling

--- src/components/bootstrap_modal.js
+++ src/components/bootstrap_modal.js
@@ -6,13 +6,13 @@ import axios from "axios"
 import "./slider-style.css"
 import Graph from "./graphshow.component"
 function Example({ hideModal, GD, showModal }) {
-// console.log(GD.length)
+  // console.log(GD.length)
   const [show, setShow] = useState(showModal);
   const [ch, setCh] = useState(-1);
 
   const [zoom, setZoom] = useState(50);
   const handleChannelId = () => {
-    axios.get("http://localhost:3000/getChannelId").then((res) => {
+    axios.get("http://192.168.70.8:3000/getChannelId").then((res) => {
       const getCh = res.data.chan
       setCh(getCh);
 
@@ -20,9 +20,9 @@ function Example({ hideModal, GD, showModal }) {
 
     })
   }
-  
+
   const handleClose = () => {
-    axios.get("http://localhost:3000/closeclient").then((response) => {
+    axios.get("http://192.168.70.8:3000/closeclient").then((response) => {
 
       console.log(response)
 
@@ -30,7 +30,7 @@ function Example({ hideModal, GD, showModal }) {
       console.log(err)
 
     })
-    hideModal(); 
+    hideModal();
     setShow(false);
 
 
@@ -41,7 +41,6 @@ function Example({ hideModal, GD, showModal }) {
   }, [GD])
   return (
     <>
-  
       {
         ch > 0 ?
           <Modal size="lg" aria-labelledby="contained-modal-title-vcenter" backdrop="static" keyboard={false} centered show={show} onHide={handleClose}  >
@@ -54,11 +53,21 @@ function Example({ hideModal, GD, showModal }) {
                 <Col lg={11} className="p-2">
                   <div id="zoom-graph" style={{ backgroundColor: "black", width: "730px", height: "400px", border: "1px solid white" }}>
                     <Graph GD={GD} zoom={zoom} /></div>
-                </Col><Col lg={1} style={{ padding: "30px" }}>
-                  <input className="pl-3" type="range" value={zoom} class="form-range" orient="vertical" onChange={(e)=>setZoom(e.target.value)} min={-251} max={251} step={25} id="customRange3"></input>
-                
- 
+
                 </Col>
+
+
+
+                <Col lg={1} style={{ padding: "30px" }}>
+                  <input className="pl-3" type="range" value={zoom} class="form-range" orient="vertical" onChange={(e)=>setZoom(e.target.value)} min={-251} max={251} step={25} id="customRange3"></input>
+
+
+
+                </Col>
+                {/* <Col lg={12}><div class="slidecontainer">
+                  <input type="range" min="1" max="100" value={zoom} onChange={(e) => setZoom(e.target.value)} class="slider" id="myRange" />
+
+                </div></Col> */}
               </Row>
             </Modal.Body>
           </Modal>
